e1416ffa9882f9811ece801a6e55ae441356b614
Romain Claret from noto.epfl.ch <romain@claret.tech>
2021-03-29 14:12:34 +0200

    + python files

--- lab3/main.py
+++ lab3/main.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-# 22.03.19
-# Assignment lab 02
+# 29.03.19
+# Assignment lab 03
 
 # Master Class: Machine Learning (5MI2018)
 # Faculty of Economic Science
 # University of Neuchatel (Switzerland)
-# Lab 2, see ML21_Exercise_1.pdf for more information
+# Lab 3, see ML21_Exercise_3.pdf for more information
 
 # https://github.com/RomainClaret/msc.ml.labs
 
@@ -13,15 +13,15 @@
 # - Romain Claret @RomainClaret
 # - Sylvain Robert-Nicoud @Nic0uds
 
-# 1. Convert attributes from nominal to numeric, using different methods.
-
 # PART: DATA CLEANING AND PREPARATION
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
 from sklearn.metrics import accuracy_score
-import matplotlib.pyplot as plt
+import pickle
 
 
 # get the features names and the values of the categories from adult.names (build a dictionary)
@@ -37,17 +37,6 @@ with open('adult.names') as f:
 header = list(data_dict.keys())+['income']
 df_train = pd.read_table("adult.data", sep=r',\s', na_values='?', header=None, names=header).dropna()
 df_evaluate = pd.read_table("adult.test", sep=r',\s', na_values='?', skiprows=[0], header=None, names=header).dropna()
-
-
-# Standardizing of numeric values
-# Doesn't have a lot of meaning in the case of decision trees as it's not using distances (like KNN)
-# But it's just a pedagological flavor
-# https://stats.stackexchange.com/questions/10289/whats-the-difference-between-normalization-and-standardization
-#for c in df_train.select_dtypes(exclude=['object']):
-#    df_train["stand_"+c] = df_train[c] - (df_train[c].mean() / df_train[c].std())
-#    
-#for c in df_evaluate.select_dtypes(exclude=['object']):
-#    df_evaluate["stand_"+c] = df_evaluate[c] - (df_evaluate[c].mean() / df_evaluate[c].std())
 
 
 # droping the education because it's redundant with education-num
@@ -125,94 +114,80 @@ for cw in cat_workclass:
     
 df_train = df_train.append(df_fake).reset_index(drop=True)
 df_evaluate = df_evaluate.append(df_fake).reset_index(drop=True)
-    
-    
-# for non-binary categories we will be using a onehot encoder as decision trees are sensitive to leaves values
-# note that get_dummies from pandas is exactly doing this without the complexity of using OneHotEncoder manually from sklearn
-# - workclass, race, native-country
-for l in ["workclass", "race", "native-country"]:
-    df_train=pd.concat([df_train, pd.get_dummies(df_train[l], prefix="encoded_"+l)], axis=1)
-    df_evaluate=pd.concat([df_evaluate, pd.get_dummies(df_evaluate[l], prefix="encoded_"+l)], axis=1)
 
+
+# get meaningful columns
+continuous_features = [k for k, v in data_dict.items() if v == ""]
+unencoded_features = ["workclass", "race", "native-country"]
+encoded_features = [c for c in df_train if c.startswith('encoded')]
+columns = continuous_features+unencoded_features+encoded_features
+    
+
+# PART PIPELINE
+# 1. Use a Pipeline for the transformation and model training.
+# Here we build out pipeline
+# First we create a ColumnTransformer for the Categorical (ignoring unknown values) and Numeric features
+# Second we pipe the features and the classifier with the parameters from lab2
+    
+# Standardizing of numeric values
+# Doesn't have a lot of meaning in the case of decision trees as it's not using distances (like KNN)
+# But it's just a pedagological flavor and to use the ColumnTranformer for whatever reason 
+# https://stats.stackexchange.com/questions/10289/whats-the-difference-between-normalization-and-standardization
+
+# We choose a the best parameters from lab2 for the decision tree
+# depth=8 Train accuracy_score 0.8550292179535
+# depth=8 Test accuracy_score 0.8465108569534229
+# depth=8 Evaluation accuracy_score 0.8469455511288181 
+
+feature_transformation = ColumnTransformer(transformers=[
+    ('categorical', OneHotEncoder(handle_unknown='ignore'), unencoded_features+encoded_features[:-1]),
+    ('numeric', StandardScaler(), continuous_features)
+])
+
+adult_pipeline = Pipeline(steps=[
+  ('features', feature_transformation),
+  ('classifier', DecisionTreeClassifier(criterion='gini', random_state=1, max_depth=8))
+])
+
+# building our pipeline-driven model
+pipeline_model = adult_pipeline.fit(df_train, df_train["encoded_income"])
     
 #remove the fake rows
 df_train = df_train[:-len(df_fake)]
 df_evaluate = df_evaluate[:-len(df_fake)]    
 
-# keep meaningful columns
-continuous_features = [k for k, v in data_dict.items() if v == ""]
-encoded_features = [c for c in df_train if c.startswith('encoded')]
-columns = continuous_features+encoded_features
-columns.remove("encoded_income")    
 
 # make training and testings sets
-X_train, X_test, y_train, y_test = train_test_split(df_train[columns],
+X_train, X_test, y_train, y_test = train_test_split(df_train,
                                                     df_train["encoded_income"],
                                                     test_size=0.2,
                                                     random_state=1)
 
 # make evaluation sets
-X_evaluate = df_evaluate[columns]
+X_evaluate = df_evaluate
 y_evaluate = df_evaluate["encoded_income"]
 
 
-# PART CLASSIFICATIONS
-# We used the training set to train our models and then we tested them on the testing set
-# We made a loop to try a parameter (depth for decision tree, and k for KNN) and select the best one based on the testing set.
-
-# run a Decision Tree Classifier
-cl_name = "Searching best Depth for Decision Tree Classifier"
-print("*"*len(cl_name))
-print(cl_name)
-print("*"*len(cl_name),'\n')
-
-parameter_dtree_min = 1
-parameter_dtree_max = 15
-preds_dtree_train=[]
-preds_dtree_test=[]
-for depth in range(parameter_dtree_min,parameter_dtree_max):
-    cl_dtree = DecisionTreeClassifier(criterion='gini', random_state=1, max_depth=depth)
-    dtree_model = cl_dtree.fit(X_train, y_train)
-    y_hat_dtree_train = dtree_model.predict(X_train)
-    y_hat_dtree_test = dtree_model.predict(X_test)
-    preds_dtree_train.append(accuracy_score(y_train, y_hat_dtree_train))
-    preds_dtree_test.append(accuracy_score(y_test, y_hat_dtree_test))
-    #print(depth,"Train accuracy_score",preds_train[-1])
-    #print(depth,"Test accuracy_score",preds_test[-1],"\n")
-    
-plt.scatter(range(parameter_dtree_min, parameter_dtree_max), preds_dtree_train, c="b", label="train score")
-plt.scatter(range(parameter_dtree_min, parameter_dtree_max), preds_dtree_test, c="r", label="test score")
-plt.legend(loc="upper left")
-plt.title('DecisionTreeClassifier: accuracy_score vs depth')
-plt.xlabel('depth')
-plt.ylabel('accuracy_score')
-plt.savefig('DecisionTreeClassifier.png')
-#plt.show()
-plt.clf()
-
-
 # PART EVALUTATION
-# We evaluate the best parameter found previously with the evaluation set (adult.test)
+# evaluate Decision Tree with our pipeline
 
-#evaluate Decision Tree on new data
-#present depth with best score for evaluation dataset
-cl_name = "Evaluate Decision Tree Classifier on new data"
+cl_name = "Evaluate Decision Tree Classifier Pipeline on new data"
 print("*"*len(cl_name))
 print(cl_name)
 print("*"*len(cl_name),'\n')
-max_dtree_index = preds_dtree_test.index(max(preds_dtree_test))
-best_depth = list(range(parameter_dtree_min, parameter_dtree_max))[max_dtree_index]
-cl_dtree = DecisionTreeClassifier(criterion='gini', random_state=1, max_depth=best_depth)
-dtree_model = cl_dtree.fit(X_train, y_train)
-y_hat_dtree_train = dtree_model.predict(X_train)
-y_hat_dtree_test = dtree_model.predict(X_test)
-y_hat_dtree_evaluate = dtree_model.predict(X_evaluate)
-print("depth="+str(best_depth),"Train accuracy_score",accuracy_score(y_train, y_hat_dtree_train))
-print("depth="+str(best_depth),"Test accuracy_score",accuracy_score(y_test, y_hat_dtree_test))
-print("depth="+str(best_depth),"Evaluation accuracy_score",accuracy_score(y_evaluate, y_hat_dtree_evaluate),"\n")
+
+y_hat_dtree_train = pipeline_model.predict(X_train)
+y_hat_dtree_test = pipeline_model.predict(X_test)
+y_hat_dtree_evaluate = pipeline_model.predict(X_evaluate)
+
+print("Train accuracy_score",accuracy_score(y_train,y_hat_dtree_train))
+print("Test accuracy_score",accuracy_score(y_test,y_hat_dtree_test))
+print("Evaluation accuracy_score",accuracy_score(y_evaluate,y_hat_dtree_evaluate),"\n")
 
 
-plt.close()
+# SERALIZE PIPELINE
+# 2. Save the Pipeline (using pickle) into a file
+pickle.dump(pipeline_model, open("pipeline_model.pickle", "wb" ))
 
 
 #def main():
---
+++ lab3/predict_income.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+# 29.03.19
+# Assignment lab 03
+
+# Master Class: Machine Learning (5MI2018)
+# Faculty of Economic Science
+# University of Neuchatel (Switzerland)
+# Lab 3, see ML21_Exercise_3.pdf for more information
+
+# https://github.com/RomainClaret/msc.ml.labs
+
+# Authors: 
+# - Romain Claret @RomainClaret
+# - Sylvain Robert-Nicoud @Nic0uds
+
+import warnings
+import pickle
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import accuracy_score
+
+warnings.filterwarnings("ignore")
+
+
+# RUN PIPELINE MODEL FROM OTHER FILE
+# 3. Create a second script that will load the Pipeline and use it to predict values from an
+#input file, and save the predictions into a different file.
+#Example:
+#Let's say you have the input data weekly in the file adult_2021_cw_12.csv.
+#This second script should read the input from this file and use the classifier to make predictions and write those predictions in the file adult_2021_cw_12_pred.csv .
+
+
+#spliting the adult.test file into several files to simulate weeks
+#filename = 'adult.test'
+#file_handler = open(filename, 'r').readlines()
+#prefix_file = "adult_2021_cw_"
+#week_number = 1
+#split_into = 10
+#line_count = 0
+#with open(filename) as f: line_count = sum(1 for _ in f)
+#
+#for i in range(len(file_handler)):
+#    if i % (line_count//split_into) == 0:
+#        open(str(prefix_file)+str(week_number) + ".csv", "w+").writelines(file_handler[i:i+1000])
+#        week_number += 1
+
+
+# load pipeline model
+pipeline_model = pickle.load( open("pipeline_model.pickle", "rb" ))
+
+weeks_count = 10
+filename = 'adult.test'
+prefix_file = "adult_2021_cw_"
+
+# get the features names and the values of the categories from adult.names (build a dictionary)
+data_dict = {}
+with open('adult.names') as f:
+    for l in f:
+        if l[0] == '|' or ':' not in l: continue
+        c = l.split(':')
+        if c[1].startswith(' continuous'): data_dict[c[0]] = ""
+        else: data_dict[c[0]] = c[1].replace("\n","").replace(".","").replace(" ","").split(",")
+            
+header = list(data_dict.keys())+['income']
+
+# for each week based on a count and a naming convention
+for i in range (weeks_count):
+    filename = str(prefix_file)+str(i+1)+".csv"
+    df_weekly = pd.read_table(filename, sep=r',\s', na_values='?', skiprows=[0], header=None, names=header).dropna()
+    
+    drop_list = ["education", "occupation", "relationship"]
+    df_weekly = df_weekly.drop(columns=drop_list)
+    
+    dict_replace = {
+    'marital-status' : {
+        'Never-married': 'Not-Married',
+        'Married-civ-spouse': 'Married',
+        'Divorced': 'Not-Married',
+        'Married-spouse-absent': 'Married',
+        'Separated': 'Married',
+        'Married-AF-spouse': 'Married',
+        'Widowed': 'Not-Married'
+        },
+    'workclass': {
+        'State-gov': 'Government',
+        'Self-emp-not-inc': 'Self-Employment',
+        'Federal-gov': 'Government',
+        'Local-gov': 'Government',
+        'Self-emp-inc': 'Self-Employment'
+        }
+    }
+
+    df_weekly.replace(dict_replace, inplace=True)
+    
+    df_weekly["income"].replace({"<=50K.": "<=50K", ">50K.": ">50K"}, inplace=True)
+    
+    for l in ["marital-status", "sex", "income"]:
+        l_enc = LabelEncoder()
+        encoder_weekly = l_enc.fit(df_weekly[l])
+        df_weekly["encoded_"+l] = encoder_weekly.transform(df_weekly[l])
+    
+    y_hat_dtree_weekly = pipeline_model.predict(df_weekly)
+    
+    pref_filename = str(prefix_file)+str(i+1)+"_pred.csv"
+    print(pref_filename, "accuracy_score:",accuracy_score(df_weekly["encoded_income"],y_hat_dtree_weekly),"\n")
+    
+    # save the prediction into file
+    pd.DataFrame(y_hat_dtree_weekly).to_csv(str(pref_filename)+str(i+1)+"_pred.csv",header=["pred_income"], index=None)
