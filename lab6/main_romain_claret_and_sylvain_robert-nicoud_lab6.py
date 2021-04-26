923ff608e279d31258e325fa1649573148e7ef50
Romain Claret from noto.epfl.ch <romain@claret.tech>
2021-04-26 09:03:20 +0200

    + initiate lab6

---
+++ lab6/main_romain_claret_and_sylvain_robert-nicoud_lab6.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# 26.04.21
+# Assignment lab 06
+
+# Master Class: Machine Learning (5MI2018)
+# Faculty of Economic Science
+# University of Neuchatel (Switzerland)
+# Lab 6, see ML21_Exercise_6.pdf for more information
+
+# https://github.com/RomainClaret/msc.ml.labs
+
+# Authors: 
+# - Romain Claret @RomainClaret
+# - Sylvain Robert-Nicoud @Nic0uds
+
+
+import pandas as pd
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+from sklearn.mixture import GaussianMixture
+import matplotlib.pyplot as plt
+
+
+# 1. Import the data and filter the two datasets for the chosen two years.
+df = pd.read_stata('pwt100.dta')
+df_1990 = df[(df["year"]==1990)]
+df_1970 = df[(df["year"]==1970)]
+
+features = [
+    "hc",
+    "ctfp",
+    "cwtfp",
+    "delta",
+    "pl_con",
+    "pl_da",
+    "pl_gdpo",
+    "csh_g",
+    "pl_c",
+    "pl_i",
+    "pl_g",
+    #"pl_k",
+]
+df_1990_cleaned = df_1990[features].dropna()
+df_1970_cleaned = df_1970[features].dropna()
+
+# 2. Choose the clustering technique and obtain the clusters for the two datasets.
+# we choose kmeans and the gausianne mixture
+# https://scikit-learn.org/stable/modules/generated/sklearn.cluster.KMeans.html
+# https://scikit-learn.org/stable/modules/generated/sklearn.mixture.GaussianMixture.html
+df_1970_pca = PCA(2).fit_transform(df_1970_cleaned)
+df_1990_pca = PCA(2).fit_transform(df_1990_cleaned)
+kmeans_1970 = KMeans(n_clusters=4, random_state=42).fit(df_1970_pca)
+kmeans_1990 = KMeans(n_clusters=4, random_state=42).fit(df_1990_pca)
+gnn_1970 = GaussianMixture(n_components=4, covariance_type='full').fit(df_1970_pca)
+
+# 3. Choose one of the years and visualize the results of the clustering. Give as much detail as possible.
+# use of kmeans
+y_kmeans_1970 = kmeans_1970.predict(df_1970_pca)
+plt.scatter(df_1970_pca[:,0], df_1970_pca[:,1], c=y_kmeans_1970, s=50, cmap='viridis')
+plt.scatter(kmeans_1970.cluster_centers_[:,0], kmeans_1970.cluster_centers_[:,1], c='blue', s=200, alpha=0.9)
+plt.show()
+
+# gausianne mixture for the comparaison
+y_gnn_1970 = gnn_1970.predict(df_1970_pca)
+plt.scatter(df_1970_pca[:,0], df_1970_pca[:,1], c=y_gnn_1970, s=50, cmap='viridis')
+plt.scatter(gnn_1970.means_[:,0], gnn_1970.means_[:,1], c='blue', s=200, alpha=0.9)
+plt.show()
---
+++ lab6/readme_romain_claret_and_sylvain_robert-nicoud_lab6.txt
@@ -0,0 +1,12 @@
+# 26.04.21
+# Assignment lab 06
+
+# Master Class: Machine Learning (5MI2018)
+# Faculty of Economic Science
+# University of Neuchatel (Switzerland)
+# Lab 6, see ML21_Exercise_6.pdf for more information
+
+# Authors: 
+# - Romain Claret @RomainClaret
+# - Sylvain Robert-Nicoud @Nic0uds
+
